4a6542d83d84f943797c183ade8830a9e26308f8
Kei Ito <kei.itof@gmail.com>
2020-08-31 15:58:39 +0900

    feat: add type checkers

--- src/is/Commit.ts
+++ src/is/Commit.ts
@@ -1,20 +1,28 @@
 import {createTypeChecker, isString} from '@nlib/typing';
+import {CommitAuthor, isCommitAuthor} from './CommitAuthor';
+import {CommitCommitter, isCommitCommitter} from './CommitCommitter';
 
 export interface Commit {
+    tag: Array<string>,
     hash: string,
     shortHash: string,
     parentHash: string,
     // shortParentHash: string,
+    author: CommitAuthor,
+    committer: CommitCommitter,
     message: string,
 }
 
 export const isCommit = createTypeChecker<Commit>(
     'Commit',
     {
+        tag: [isString],
         hash: isString,
         shortHash: isString,
         parentHash: isString,
         // shortParentHash: isString,
+        author: isCommitAuthor,
+        committer: isCommitCommitter,
         message: isString,
     },
 );
