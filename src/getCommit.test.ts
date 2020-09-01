54dccf177fa5cde469b01454e9cb530a242c3156
Kei Ito <kei.itof@gmail.com>
2020-09-01 12:16:41 +0900

    test: rewrite tests to use sample commits

--- src/getCommit.test.ts
+++ src/getCommit.test.ts
@@ -1,28 +1,16 @@
 import {testFunction} from '@nlib/test';
 import {getCommit} from './getCommit';
-import {Date} from '@nlib/global';
+import {
+    thirdCommit,
+    thirdCommitLike,
+    secondCommit,
+    firstCommit,
+} from './sample.test';
 
-testFunction(getCommit, {
-    input: 'b3ec2a662d1adf4d040960bd5ae40b2d7e308418',
-    expected: {
-        tag: ['tag-2', 'tag-1'],
-        reference: [],
-        hash: 'b3ec2a662d1adf4d040960bd5ae40b2d7e308418',
-        shortHash: 'b3ec2a6',
-        parentHash: '09261b7cd053b3b2c0a75bbe21266c1843768fec',
-        author: {
-            date: new Date('2020-08-31T03:07:01+09:00'),
-            name: 'Kei Ito',
-            email: ['kei.itof', 'gmail.com'].join('@'),
-        },
-        committer: {
-            date: new Date('2020-08-31T03:07:01+09:00'),
-            name: 'Kei Ito',
-            email: ['kei.itof', 'gmail.com'].join('@'),
-        },
-        message: [
-            'feat: add the "exec" function',
-            'this message includes some \'\\special\' characters😉',
-        ].join('\n'),
-    },
-});
+testFunction(getCommit, {input: thirdCommit.hash, like: thirdCommitLike});
+
+testFunction(getCommit, {input: secondCommit.hash, expected: secondCommit});
+testFunction(getCommit, {input: secondCommit.shortHash, expected: secondCommit});
+testFunction(getCommit, {input: secondCommit.tag[0], expected: secondCommit});
+
+testFunction(getCommit, {input: firstCommit.hash, expected: firstCommit});
--- src/walkCommitHistory.test.ts
+++ src/walkCommitHistory.test.ts
@@ -1,27 +1,15 @@
 import ava from 'ava';
 import {walkCommitHistory} from './walkCommitHistory';
+import {thirdCommit, thirdCommitLike, secondCommit, firstCommit} from './sample.test';
 
 ava('walk commit history', async (t) => {
-    const thirdCommitish = '4f25e7e3672d591fbb379790192d0b035889a5e2';
-    const asyncIterator = walkCommitHistory(thirdCommitish);
-    const thirdCommit = await asyncIterator.next();
-    t.like(thirdCommit.value, {
-        hash: '4f25e7e3672d591fbb379790192d0b035889a5e2',
-        parentHash: 'b3ec2a662d1adf4d040960bd5ae40b2d7e308418',
-    });
-    t.log(thirdCommit.value);
-    const secondCommit = await asyncIterator.next();
-    t.like(secondCommit.value, {
-        hash: 'b3ec2a662d1adf4d040960bd5ae40b2d7e308418',
-        parentHash: '09261b7cd053b3b2c0a75bbe21266c1843768fec',
-    });
-    t.log(secondCommit.value);
-    const firstCommit = await asyncIterator.next();
-    t.like(firstCommit.value, {
-        hash: '09261b7cd053b3b2c0a75bbe21266c1843768fec',
-        parentHash: '',
-    });
-    t.log(firstCommit.value);
+    const asyncIterator = walkCommitHistory(thirdCommit.hash);
+    const third = await asyncIterator.next();
+    t.like(third.value, thirdCommitLike);
+    const second = await asyncIterator.next();
+    t.deepEqual(second.value, secondCommit);
+    const first = await asyncIterator.next();
+    t.deepEqual(first.value, firstCommit);
     const done = await asyncIterator.next();
     t.true(done.done);
 });
