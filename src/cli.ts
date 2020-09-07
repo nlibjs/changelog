0924ed397327ad680dc8630593c851ed7e7e0263
Kei Ito <kei.itof@gmail.com>
2020-09-07 14:37:58 +0900

    feat: improve cli using nlib/nodetool

--- src/cli.ts
+++ src/cli.ts
@@ -3,35 +3,60 @@ import * as process from 'process';
 import * as console from 'console';
 import * as path from 'path';
 import * as fs from 'fs';
-import {JSON} from '@nlib/global';
+import {
+    createCLIArgumentsParser,
+    getVersion,
+    serializeDefinitionMap,
+} from '@nlib/nodetool';
 import {generateChangelog} from './generateChangelog';
 
-const execute = async (
+const parse = createCLIArgumentsParser({
+    output: {
+        type: 'string',
+        alias: 'o',
+        description: 'A file path nlib-changelog writes to',
+    },
+    head: {
+        type: 'string?',
+        description: 'Specify the head commitish',
+    },
+    help: {
+        type: 'boolean',
+        alias: 'h',
+        description: 'Show help',
+    },
+    version: {
+        type: 'boolean',
+        alias: 'v',
+        description: 'Output the version number',
+    },
+});
+
+const nlibChangelogCLI = async (
+    args: Array<string>,
+    stdout: NodeJS.WritableStream = process.stdout,
 ) => {
-    const output = fs.createWriteStream('CHANGELOG.md');
-    for await (const fragment of generateChangelog()) {
-        output.write(fragment);
+    if (args.includes('--help') || args.includes('-h')) {
+        stdout.write('cleanup-package-json --file path/to/package.json\n\n');
+        for (const help of serializeDefinitionMap(parse.definition)) {
+            stdout.write(help);
+        }
+    } else if (args.includes('--version') || args.includes('-v')) {
+        stdout.write(`${getVersion(path.join(__dirname, '../package.json'))}\n`);
+    } else {
+        const props = parse(args);
+        const output = fs.createWriteStream(path.resolve(props.output));
+        for await (const fragment of generateChangelog({headCommit: props.head || 'HEAD'})) {
+            output.write(fragment);
+        }
+        output.close();
     }
-    output.close();
 };
 
 if (!module.parent) {
-    const args = process.argv.slice(2);
-    if (args.includes('--help') || args.includes('-h')) {
-        console.log([
-            'nlib-changelog [options]',
-            '-h, --help     Show help',
-            '-v, --version  Output the version number',
-        ].join('\n'));
-    } else if (args.includes('--version') || args.includes('-v')) {
-        const jsonPath = path.join(__dirname, '../package.json');
-        const {version} = JSON.parse(fs.readFileSync(jsonPath, 'utf8')) as unknown as {version: string};
-        console.log(version);
-    } else {
-        execute()
-        .catch((error) => {
-            console.error(error);
-            process.exit(1);
-        });
-    }
+    nlibChangelogCLI(process.argv.slice(2))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
 }
