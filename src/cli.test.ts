1744f8c58bc9b86de3188a32540e2ba34892f119
Kei Ito <kei.itof@gmail.com>
2020-10-04 13:33:13 +0900

    test: --head

--- src/cli.test.ts
+++ src/cli.test.ts
@@ -5,5 +5,11 @@ const scriptPath = path.join(__dirname, 'cli.ts');
 
 ava('Generate a changelog', async (t) => {
     const {stdout} = await exec(`npx ts-node ${scriptPath}`);
+    t.true(stdout.includes('## v0.1.1 (2020-09-07)'));
+    t.true(stdout.includes('## v0.1.0 (2020-09-07)'));
+});
+ava('Generate a changelog before tag-1', async (t) => {
+    const {stdout} = await exec(`npx ts-node ${scriptPath} --head v0.1.0`);
+    t.false(stdout.includes('## v0.1.1 (2020-09-07)'));
     t.true(stdout.includes('## v0.1.0 (2020-09-07)'));
 });
